dd4bcebc0f2a02bec5396fc7273176f2789ee63f
xiaoanise <61268911+xiaoanise@users.noreply.github.com>
2022-01-29 12:11:18 +0900

    商品情報編集機能を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,8 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id
-    else
+    unless @item.user_id == current_user.id
       redirect_to root_path
     end
   end
